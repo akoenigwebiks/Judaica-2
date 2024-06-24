c77d04f71ae5db414b6b48ec66204b230097fb1f
Avi E Koenig <darkmift@gmail.com>
2024-06-24 15:53:22 +0300

    added category list view

--- Judaica-2/Controllers/HomeController.cs
+++ Judaica-2/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Judaica_2.Data;
 using Judaica_2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,18 @@ namespace Judaica_2.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Ctx _context; // Add a private field for Ctx
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Ctx context) // Inject Ctx here
         {
             _logger = logger;
+            _context = context; // Assign the injected context to the private field
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
         }
 
         public IActionResult Privacy()
--- Judaica-2/Program.cs
+++ Judaica-2/Program.cs
@@ -1,7 +1,10 @@
+using Judaica_2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Ctx>();
 
 var app = builder.Build();
 
