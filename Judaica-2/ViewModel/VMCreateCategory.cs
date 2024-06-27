fe74e494d39ed6dd10cbd557c009637c3edece57
Avi E Koenig <darkmift@gmail.com>
2024-06-27 13:37:38 +0300

    added view model

---
+++ Judaica-2/ViewModel/VMCreateCategory.cs
@@ -0,0 +1,32 @@
+﻿using Judaica_2.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Judaica_2.ViewModel
+{
+    public class VMCreateCategory
+    {
+        public VMCreateCategory() { 
+            Item = new Item(); 
+            Category = new Category(); 
+            Parent = new Category(); 
+            Categories = new List<Category>();
+            Price = 0;
+        }
+        public Category Parent { get; set; }
+        [Display(Name = "שיוך לקבוצה")]
+        public int ParentID { get; set; }
+
+        public List<Category> Categories { get; set; }
+        public Category Category { get; set; }
+        public Item Item { get; set; }
+
+        [Display(Name = "הכנסת תמונה לקבוצה החדשה")]
+        public IFormFile? Image { get; set; }
+
+        [Display(Name = "הכנסת תמונה לפריט הראשון בקבוצה")]
+        public IFormFile? ImageItem { get; set; }
+
+        [Display(Name = "מחיר")]
+        public decimal Price { get; set; }
+    }
+}
