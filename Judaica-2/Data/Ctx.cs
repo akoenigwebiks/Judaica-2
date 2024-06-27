3312553e23a1a6a3eef37d77fa689493aa0977b1
Avi E Koenig <darkmift@gmail.com>
2024-06-27 13:37:10 +0300

    refiend seed logic

--- Judaica-2/Data/Ctx.cs
+++ Judaica-2/Data/Ctx.cs
@@ -6,14 +6,58 @@ namespace Judaica_2.Data
     {
         public Ctx()
         {
+            this.Database.EnsureCreated();
             Seed();
         }
 
         private void Seed()
         {
-            if (Categories.Count() > 0) return;
-            Category category = new Category { Name = "חנות יודאיקה הכי שווה בעולם" };
+            if (Categories.Any() || Items.Any() || Prices.Any()) return;
+
+            string parentCategoryName = "חנות יודאיקה הכי שווה בעולם";
+            string[] subCateogries = [
+                "ספרי תורה",
+                "תפילין",
+                "מזוזות",
+                "כיפות",
+                "סידורים"
+                ];
+            // add categories
+
+            Category category = new Category { Name = parentCategoryName };
             Categories.Add(category);
+            int[] prices = { 100, 200, 300, 400, 500 };
+            /*
+            subCateogries.ToList().ForEach(c =>
+            {
+                Category category1 = new Category { Name = c, Parent = category };
+                Categories.Add(category1);
+                int i = 0;
+                subCateogries
+                    .ToList()
+                    .ForEach((cName) =>
+                            {
+                                Item item = new Item
+                                {
+                                    Name = cName + i++,
+                                    Category = category1
+                                };
+                                Items.Add(item);
+
+                                Prices.AddRange(prices.Select(p => new Price { MyPrice = p, Item = item }));
+                            });
+            });
+            */
+            // sub-cateogries
+            List<Category> sb = subCateogries.Select(c => new Category { Name = c, Parent = category }).ToList();
+            Categories.AddRange(sb);
+
+            List<Item> items = sb.Select(c => new Item { Name = c.Name, Category = c }).ToList();
+            Items.AddRange(items);
+
+            List<Price> itemPrices = items.SelectMany(i => prices.Select(p => new Price { MyPrice = p, Item = i })).ToList();
+            Prices.AddRange(itemPrices);
+
             SaveChanges();
         }
 
